d89259833be73edec688a24b1af2122b1f31308a
Anand S <36130650+ClobberXD@users.noreply.github.com>
2018-04-10 15:47:05 +0530

    Repo-wide typo-fix
    Aluminum -> Aluminium (correct English and IUPAC Approved).
    
    The latter was also my original spelling, until Notepad++ gave me that ugly squiggle.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# aluminum
+# aluminium
 #### WIP (download at your own risk - this may eat up your computer)
 
 ### Dependencies: default
 
 ## Description
 
-This mod adds the metal aluminum of types:
+This mod adds the metal aluminium of types:
 - ores
 - lump
 - ingot
 - block
 
-At present, nothing new can be created with aluminum (of any type), but I've plans - lots of 'em! ;)
+At present, nothing new can be created with aluminium (of any type), but I've plans - lots of 'em! ;)
 
 ### TODO
-- Add more crafts that use aluminum
+- Add more crafts that use aluminium
--- init.lua
+++ init.lua
@@ -1,46 +1,46 @@
-minetest.register_craftitem("aluminum:aluminum_lump",{
+minetest.register_craftitem("aluminium:aluminium_lump",{
 	description = "Aluminium Lump",
-	inventory_image = "aluminum_aluminum_lump.png"
+	inventory_image = "aluminium_aluminium_lump.png"
 })
 
 minetest.register_craft({
 	type = "cooking",
-	output = "aluminum:aluminum_ingot",
-	recipe = "aluminum:aluminum_lump"
+	output = "aluminium:aluminium_ingot",
+	recipe = "aluminium:aluminium_lump"
 })
 
-minetest.register_node("aluminum:stone_with_aluminum",{
-	description = "Aluminum Ore",
-	tiles = {"default_stone.png^aluminum_mineral_aluminum.png"},
+minetest.register_node("aluminium:stone_with_aluminium",{
+	description = "Aluminium Ore",
+	tiles = {"default_stone.png^aluminium_mineral_aluminium.png"},
 	groups = {cracky = 2},
-	drop = "aluminum:aluminum_lump",
+	drop = "aluminium:aluminium_lump",
 	sounds = default.node_sound_stone_defaults()
 })
 
-minetest.register_craftitem("aluminum:aluminum_ingot",{
-	description = "Aluminum Ingot",
-	inventory_image = "aluminum_aluminum_ingot.png"
+minetest.register_craftitem("aluminium:aluminium_ingot",{
+	description = "Aluminium Ingot",
+	inventory_image = "aluminium_aluminium_ingot.png"
 })
 
-minetest.register_node("aluminum:aluminum_block",{
-	description="Aluminum Block",
-	tiles={"aluminum_aluminum_block.png"},
+minetest.register_node("aluminium:aluminium_block",{
+	description="Aluminium Block",
+	tiles={"aluminium_aluminium_block.png"},
 	is_ground_content = false,
 	groups = {cracky = 1, level = 2},
 	sounds = default.node_sound_metal_defaults()
 })
 
 minetest.register_craft({
-	output = "aluminum:aluminum_block",
+	output = "aluminium:aluminium_block",
 	recipe = {
-		{"aluminum:aluminum_ingot", "aluminum:aluminum_ingot", "aluminum:aluminum_ingot"},
-		{"aluminum:aluminum_ingot", "aluminum:aluminum_ingot", "aluminum:aluminum_ingot"},
-		{"aluminum:aluminum_ingot", "aluminum:aluminum_ingot", "aluminum:aluminum_ingot"},
+		{"aluminium:aluminium_ingot", "aluminium:aluminium_ingot", "aluminium:aluminium_ingot"},
+		{"aluminium:aluminium_ingot", "aluminium:aluminium_ingot", "aluminium:aluminium_ingot"},
+		{"aluminium:aluminium_ingot", "aluminium:aluminium_ingot", "aluminium:aluminium_ingot"},
 	}
 })
 
 minetest.register_craft({
-	output = "aluminum:aluminum_ingot 9",
+	output = "aluminium:aluminium_ingot 9",
 	type = "shapeless",
-	recipe = "aluminum:aluminum_block"
+	recipe = "aluminium:aluminium_block"
 })
